f0b420b970bee033edfd0478b674152b9240507d
njarvis <npjarvis@uw.edu>
2014-11-04 11:33:23 -0800

    created controllers for name input and movie pop.

--- js/app.js
+++ js/app.js
@@ -5,3 +5,18 @@
 
 "use strict";
 
+// everything starts by declaring a module
+angular.module('AngularDemo', [])
+    // returns a bunch of stuff to work such as controller
+    .controller('HelloController', function($scope){
+        // angular scope holds all data models that we want available in the html views
+        //added new object to scope with two properties: firstName, lastName
+        $scope.person = {
+            firstName: null,
+            lastName: null
+        };
+    })
+    .controller('MoviesController', function($scope) {
+        $scope.movies = movies;
+        // create row in html for each movie using angular
+    });
